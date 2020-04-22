5dc86704e21d24a064a632992af5768607997f35
Jagpreet2108 <jagpreetkaur2108@gmail.com>
2020-04-22 21:05:33 +0530

    FirstCommit

---
+++ Invent/Migrations/20200422112913_FirstMigration.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Invent.Migrations
+{
+    public partial class FirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Shopkeeper",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductName = table.Column<string>(nullable: false),
+                    ProductPrice = table.Column<int>(nullable: false),
+                    Quantity = table.Column<int>(nullable: false),
+                    Amount = table.Column<int>(nullable: false),
+                    Date = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Shopkeeper", x => x.ProductId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Shopkeeper",
+                columns: new[] { "ProductId", "Amount", "Date", "ProductName", "ProductPrice", "Quantity" },
+                values: new object[] { 1, 0, new DateTime(2020, 4, 20, 0, 0, 0, 0, DateTimeKind.Unspecified), "Usha Celling Fan", 550, 10 });
+
+            migrationBuilder.InsertData(
+                table: "Shopkeeper",
+                columns: new[] { "ProductId", "Amount", "Date", "ProductName", "ProductPrice", "Quantity" },
+                values: new object[] { 2, 0, new DateTime(2020, 4, 21, 0, 0, 0, 0, DateTimeKind.Unspecified), "Usha Mixer Grinder", 800, 10 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Shopkeeper");
+        }
+    }
+}
---
+++ Invent/Models/Shopkeeper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Invent.Models
+{
+    [Table("Shopkeeper")]
+    public class Shopkeeper
+    {
+        int Amount;
+
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required(ErrorMessage = "Enter Name of the Product")]
+        public string ProductName { get; set; }
+
+        [Required(ErrorMessage = "Enter Price of the Product")]
+        public int ProductPrice { get; set; }
+
+        [Required(ErrorMessage = "Enter Quantity of the Product")]
+        public int Quantity { get; set; }   
+        
+        public int amount {
+
+            get { return Amount; }
+            set
+            {
+                if (value == 0)
+                {
+                    Amount = ProductPrice * Quantity;
+                }
+
+                else
+                {
+                    Amount = value;
+                }
+            }
+
+        }
+
+        [Required(ErrorMessage = "Enter Purchase Date of the Product")]
+        public DateTime Date { get; set; }
+
+    }
+}
